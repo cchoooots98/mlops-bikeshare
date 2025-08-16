b45297ee7165434d80a6fda6904b8c1a3aaaf2cc
cchoooots98 <yyfeng2626@gmail.com>
2025-08-16 02:07:19 -0400

    fix terraform problems

--- infra/terraform/provider.tf
+++ infra/terraform/provider.tf
@@ -1,3 +1,10 @@
 provider "aws" {
   region = var.aws_region
+   default_tags {
+    tags = {
+      Project     = var.repo_name
+      Environment = var.env
+      ManagedBy   = "terraform"
+    }
+  }
 }
--- infra/terraform/variables.tf
+++ infra/terraform/variables.tf
@@ -20,3 +20,24 @@ variable "role_name" {
   description = "IAM role name for GitHub OIDC"
   default     = "gh-oidc-deployer"
 }
+
+variable "env" {
+  type    = string
+  default = "dev"
+}
+
+# 下面三项虽然当前 backend.tf 没有引用，但保留做参数化也可以。
+variable "tf_state_bucket" {
+  type    = string
+  default = "mlops-tfstate-387706002632-ca-central-1"
+}
+
+variable "tf_lock_table" {
+  type    = string
+  default = "mlops-tflock"
+}
+
+variable "tf_state_key" {
+  type    = string
+  default = "infra/terraform.tfstate"
+}
